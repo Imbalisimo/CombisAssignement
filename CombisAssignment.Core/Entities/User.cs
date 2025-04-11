cd92e674a3f1222ff9c84c6182c18f46284c43eb
Marin Pepeljnjak <marin9619@hotmail.com>
2025-04-11 20:54:25 +0200

    Add models and dbContext

---
+++ CombisAssignment.Core/Entities/User.cs
@@ -0,0 +1,26 @@
+﻿using CombisAssignment.Core.Enums;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CombisAssignment.Core.Entities
+{
+    public class User
+    {
+        public Guid Id { get; set; } = Guid.NewGuid();
+        public string Name { get; set; }
+        public string Email { get; set; }
+
+        private string _password;
+        public string Password
+        {
+            get => _password;
+            set => _password = BCrypt.Net.BCrypt.HashPassword(value);
+        }
+
+        public Role Role { get; set; }
+    }
+}
---
+++ CombisAssignment.EFCore/CombisDbContext.cs
@@ -0,0 +1,20 @@
+﻿using CombisAssignment.Core.Entities;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CombisAssignment.Core
+{
+    public class AppDbContext : DbContext
+    {
+        public DbSet<User> Users { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Server=.;Database=CombisAssignmentDb;Trusted_Connection=True;");
+        }
+    }
+}
